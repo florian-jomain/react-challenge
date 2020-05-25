48f58b3fe4fd30229a0512e5b811f8b95e6583ca
Florian Jomain <florian.jomain@gmail.com>
2020-05-25 16:23:35 +0200

    feat: committing temperature component

--- src/components/Temperature.jsx
+++ src/components/Temperature.jsx
@@ -6,11 +6,60 @@ export class Temperature extends React.Component {
     temperature: 0,
   };
 
+  handleChange = (event) => {
+    this.setState({ temperature: event.target.value });
+  };
+
   render() {
+    const { temperature } = this.state;
+
     return (
-      <div>
+      <React.Fragment>
         <NavBar />
-      </div>
+        <div className='Temperature'>
+          <div className='Temperature__group'>
+            <h1 className='Temperature__header'>Temperature</h1>
+            <input
+              onChange={this.handleChange}
+              className='Temperature__input'
+              type='number'
+              placeholder='Temperature in °C'
+              min='0'
+              name='temperature'
+            />
+            {temperature < 10 && (
+              <div className='Temperature__result'>
+                <p style={{ color: "blue" }}>
+                  It's cold{" "}
+                  <span role='img' aria-label='snow'>
+                    ❄️
+                  </span>
+                </p>
+              </div>
+            )}
+            {temperature >= 10 && temperature <= 30 && (
+              <div className='Temperature__result'>
+                <p style={{ color: "black" }}>
+                  It's nice{" "}
+                  <span role='img' aria-label='flower'>
+                    🌼
+                  </span>
+                </p>
+              </div>
+            )}
+            {temperature > 30 && (
+              <div className='Temperature__result'>
+                <p style={{ color: "red" }}>
+                  It's warm{" "}
+                  <span role='img' aria-label='snow'>
+                    ☀️
+                  </span>
+                </p>
+              </div>
+            )}
+          </div>
+        </div>
+      </React.Fragment>
     );
   }
 }
